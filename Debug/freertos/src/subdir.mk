2fe3696a265a913a7ea9bf47a391a6e6d77d0cc2
FuryFire <furyfire@users.noreply.github.com>
2018-03-05 12:41:19 +0100

    Added task 2 from Mortens instructions

--- Debug/freertos/src/subdir.mk
+++ Debug/freertos/src/subdir.mk
@@ -32,7 +32,7 @@ C_DEPS += \
 freertos/src/%.o: ../freertos/src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M4 -DSDK_DEBUGCONSOLE=0 -I"/home/tobias/Documents/MCUXpresso_10.1.0_589/workspace/lpc_chip_40xx/inc" -I"/home/tobias/Documents/MCUXpresso_10.1.0_589/workspace/lpc_board_ea_devkit_4088/inc" -I"/home/tobias/Documents/MCUXpresso_10.1.0_589/workspace/FreeRTOS_Semaphore/example/inc" -I"/home/tobias/Documents/MCUXpresso_10.1.0_589/workspace/FreeRTOS_Semaphore/freertos/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fsingle-precision-constant -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M4 -DSDK_DEBUGCONSOLE=0 -I"C:\Users\JCT\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc" -I"C:\Users\JCT\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_board_ea_devkit_4088\inc" -I"C:\Users\JCT\Documents\MCUXpressoIDE_10.0.2_411\workspace\freertos_blinky_ms_timer\example\inc" -I"C:\Users\JCT\Documents\MCUXpressoIDE_10.0.2_411\workspace\freertos_blinky_ms_timer\freertos\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fsingle-precision-constant -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- example/src/main.c
+++ example/src/main.c
@@ -33,13 +33,31 @@
 #include "FreeRTOS.h"
 #include "semphr.h"
 #include "task.h"
+
 #include "ms_timer.h"
 
 xSemaphoreHandle second_isr_semaphore;
 
+// pins 5-8
+#define GPIO_PORT1 1
+#define GPIO_PIN1 24
+#define GPIO_PORT2 1
+#define GPIO_PIN2 23
+#define GPIO_PORT3 1
+#define GPIO_PIN3 20
+#define GPIO_PORT4 0
+#define GPIO_PIN4 21
+
 static void second_callback_isr() {
 	//Special semaphore function to be used inside ISR's
-	xSemaphoreGiveFromISR(second_isr_semaphore,NULL);
+	long task_woken;
+	xSemaphoreGiveFromISR(second_isr_semaphore, &task_woken);
+	if(task_woken) {
+		Chip_GPIO_WritePortBit(LPC_GPIO, GPIO_PORT2, GPIO_PIN2, 1);
+		Board_LED_Toggle(3);
+		Chip_GPIO_WritePortBit(LPC_GPIO, GPIO_PORT2, GPIO_PIN2, 0);
+		taskYIELD();
+	}
 }
 
 /* Sets up system hardware */
@@ -48,18 +66,41 @@ static void prvSetupHardware(void)
 	SystemCoreClockUpdate();
 	Board_Init();
 
+	Chip_IOCON_PinMuxSet(LPC_IOCON, GPIO_PORT1, GPIO_PIN1, IOCON_FUNC0);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO, GPIO_PORT1, GPIO_PIN1);
+
+	Chip_IOCON_PinMuxSet(LPC_IOCON, GPIO_PORT2, GPIO_PIN2, IOCON_FUNC0);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO, GPIO_PORT2, GPIO_PIN2);
+
+	Chip_IOCON_PinMuxSet(LPC_IOCON, GPIO_PORT3, GPIO_PIN3, IOCON_FUNC0);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO, GPIO_PORT3, GPIO_PIN3);
+
+	Chip_IOCON_PinMuxSet(LPC_IOCON, GPIO_PORT4, GPIO_PIN4, IOCON_FUNC0);
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO, GPIO_PORT4, GPIO_PIN4);
+
 	ms_timer_init (second_callback_isr);
+
+
 }
 
 /* Semaphore task. It is given a semaphore each second after a interrupt. */
 static void sem_task(void *pvParameters) {
-	int tickCnt = 0;
-
 	while (1) {
 		if(xSemaphoreTake(second_isr_semaphore, portMAX_DELAY)) {
-			DEBUGOUT("Tick: %d\r\n", tickCnt);
-			tickCnt++;
+			Board_LED_Toggle(1);
 		}
+	}
+}
+
+/* Low priority tasks... Tries to take a lot of CPU time*/
+static void lowpri_task(void *pvParameters) {
+	while (1) {
+		Board_LED_Toggle(2);
+		Chip_GPIO_WritePortBit(LPC_GPIO, GPIO_PORT1, GPIO_PIN1, 1);
+		for(long i = 0; i <6000000; i++);
+		Chip_GPIO_WritePortBit(LPC_GPIO, GPIO_PORT1, GPIO_PIN1, 0);
+		Board_LED_Toggle(2);
+		vTaskDelay(configTICK_RATE_HZ / 20); //~10Hz
 	}
 }
 
@@ -75,9 +116,11 @@ int main(void)
 
 	// Semaphore tasks. Prints ticks every second after receiving semaphore.
 	xTaskCreate(sem_task, (signed char *) "sem_task",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 2UL),
+				(xTaskHandle *) NULL);
+	xTaskCreate(lowpri_task, (signed char *) "lowpri_task",
 				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
 				(xTaskHandle *) NULL);
-
 	/* Start the scheduler */
 	vTaskStartScheduler();
 
